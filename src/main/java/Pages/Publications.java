25169ffcc61fadc00108fa3c95901c8d0549b406
Surendar Kaliappan <Surendar@embibe.com>
2019-12-31 17:48:37 +0530

    Publications 10 TestCases

--- src/main/java/Pages/Publications.java
+++ src/main/java/Pages/Publications.java
@@ -3,14 +3,12 @@ package Pages;
 import base.TestBase;
 import constants.TimeDelay;
 import io.qameta.allure.Step;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 
+import java.util.List;
 import java.util.Random;
 
 public class Publications extends TestBase {
@@ -21,7 +19,7 @@ public class Publications extends TestBase {
 
     @FindBy(id = "metatagging_publication_name")
     WebElement publicationName;
-    @FindBy(id="metatagging_publication_code")
+    @FindBy(id = "metatagging_publication_code")
     WebElement publicationCode;
     @FindBy(id = "metatagging_publication_description")
     WebElement publicationDescription;
@@ -34,36 +32,34 @@ public class Publications extends TestBase {
 
     @FindBy(id = "error_explanation")
     WebElement errorMessage;
-    @FindBy(className = "alert alert-success alert-dismissable")
-    WebElement successValidationMessage;
     @FindBy(id = "publications_table_wrapper")
     WebElement listingTable;
 
     @Step("Click Publications on Side Bar")
-    public void openPublicaton(){
+    public void openPublicaton() {
+        waitUntillElementVisible(driver.findElement(By.xpath("//*[contains(text(),'Publications')]")));
         driver.findElement(By.xpath("//*[contains(text(),'Publications')]")).click();
     }
 
     @Step("Click on Save Button")
-    public void clkSaveBtn(){
-        delayTime(TimeDelay.TIME_2000S);
-        driver.findElement(By.xpath("//*[contains(text(),'Save')]")).submit();
-        //driver.findElement(By.className("btn btn-primary btn-block m-t")).submit();
+    public void clkSaveBtn() {
+        pageScrole(driver.findElement(By.xpath("//*[@value='Save']")));
+        driver.findElement(By.xpath("//*[@value='Save']")).submit();
     }
 
     @Step("Verify the URL of publication")
-    public void verifyPublicationPageURL(){
+    public void verifyPublicationPageURL() {
         String currentURL = driver.getCurrentUrl();
-        Assert.assertEquals(currentURL,"https://new-content-admin-staging.embibe.com/publications");
+        Assert.assertEquals(currentURL, "https://new-content-admin-staging.embibe.com/publications");
     }
 
     @Step("Click Add New Publication Button")
-    public void clkAddNewPublicationBtn(){
+    public void clkAddNewPublicationBtn() {
         driver.findElement(By.xpath("//*[@class=\"btn btn-primary btn-sm\"]")).click();
     }
 
     @Step("Verify all the fields available")
-    public void verifyAllFiledAvailable(){
+    public void verifyAllFiledAvailable() {
         Assert.assertTrue(publicationName.isDisplayed());
         Assert.assertTrue(publicationCode.isDisplayed());
         Assert.assertTrue(publicationDescription.isDisplayed());
@@ -73,94 +69,110 @@ public class Publications extends TestBase {
     }
 
     @Step("Enter Publication Name")
-    public void enterPublicationName(String name){
+    public void enterPublicationName(String name) {
+        delayTime(TimeDelay.TIME_1000S);
         publicationName.sendKeys(name);
     }
+
     @Step("Enter Unique Publication Name")
-    public void enterUniquePublicationName(String name){
+    public void enterUniquePublicationName(String name) {
         delayTime(TimeDelay.TIME_1000S);
         Random objGenerator = new Random();
         for (int iCount = 0; iCount < 1; iCount++) {
             int randomNumber = objGenerator.nextInt(100000000);
-            publicationName.sendKeys(name+" "+ randomNumber);
+            publicationName.sendKeys(name + " " + randomNumber);
         }
     }
 
     @Step("Enter Publication Code")
-    public void enterPublicationCode(String code){
-            publicationCode.sendKeys(code);
+    public void enterPublicationCode(String code) {
+        delayTime(TimeDelay.TIME_1000S);
+        publicationCode.sendKeys(code);
     }
 
     @Step("Enter Unique Publication Code")
-    public void enterUniquePublicationCode(String code){
+    public void enterUniquePublicationCode(String code) {
         Random objGenerator = new Random();
         for (int iCount = 0; iCount < 1; iCount++) {
             int randomNumber = objGenerator.nextInt(100000000);
-            publicationCode.sendKeys(code+" "+randomNumber);
+            publicationCode.sendKeys(code + " " + randomNumber);
         }
     }
 
     @Step("Enter Publication Description")
-    public void enterPublicationDescription(String description){
+    public void enterPublicationDescription(String description) {
+        delayTime(TimeDelay.TIME_1000S);
         publicationDescription.sendKeys(description);
     }
 
     @Step("Enter Publication Address")
-    public void enterPublicationAddress(String address){
+    public void enterPublicationAddress(String address) {
+        delayTime(TimeDelay.TIME_1000S);
         publicationAddress.sendKeys(address);
     }
 
     @Step("Enter Publication Contact Number")
-    public void enterPublicationContactNumber(String contactNumber){
+    public void enterPublicationContactNumber(String contactNumber) {
+        delayTime(TimeDelay.TIME_1000S);
         publicationContactNumber.sendKeys(contactNumber);
     }
 
     @Step("Select Publication Country")
-    public void selectPublicationCountry(String countryName){
-        WebElement countrySearchField = driver.findElement(By.id("select2-metatagging_publication_country-container"));
-        countrySearchField.click();
+    public void selectPublicationCountry(String countryName) {
+        delayTime(TimeDelay.TIME_1000S);
+        driver.findElement(By.id("select2-metatagging_publication_country-container")).click();
         driver.findElement(By.className("select2-search__field")).sendKeys(countryName);
         driver.findElement(By.className("select2-search__field")).sendKeys(Keys.ENTER);
-        //countrySearchField.sendKeys(Keys.ENTER);
     }
 
     @Step("Verify Validation Message")
-    public void verifyValidationMessage(){
+    public void verifyValidationMessage() {
         listingTable.isDisplayed();
-        Assert.assertTrue(successValidationMessage.isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//*[@class='alert alert-success alert-dismissable']")).isDisplayed());
     }
 
     @Step("Verify Error Message")
-    public void verifyErrorMessage(){
+    public void verifyErrorMessage() {
+        delayTime(TimeDelay.TIME_2000S);
         Assert.assertTrue(errorMessage.isDisplayed());
     }
 
-    public void verifyNoErrorWhileSaving(){
-        Assert.assertFalse(errorMessage.isDisplayed());
-    }
-
     @Step("Verify Publication Name")
-    public void verifyPublicationName(String expectedName){
+    public void verifyPublicationName(String expectedName) {
+        delayTime(TimeDelay.TIME_2000S);
         Assert.assertEquals(publicationName.getAttribute("value"), expectedName);
     }
 
     @Step("Verify Publication Code")
-    public void verifyPublicationCode(String expectedCode){
+    public void verifyPublicationCode(String expectedCode) {
+        delayTime(TimeDelay.TIME_1000S);
         Assert.assertEquals(publicationCode.getAttribute("value"), expectedCode);
     }
 
     @Step("Verify Publication Description")
-    public void verifyPublicationDescription(String expectedDescription){
+    public void verifyPublicationDescription(String expectedDescription) {
+        delayTime(TimeDelay.TIME_1000S);
         Assert.assertEquals(publicationDescription.getAttribute("value"), expectedDescription);
     }
 
     @Step("Verify Publication Address")
-    public void verifyPublicationAddress(String expectedAddress){
+    public void verifyPublicationAddress(String expectedAddress) {
+        delayTime(TimeDelay.TIME_1000S);
         Assert.assertEquals(publicationAddress.getAttribute("value"), expectedAddress);
     }
 
     @Step("Verify Publication Contact Number")
-    public void verifyPublicationContactNumber(String expectedContactNumber){
+    public void verifyPublicationContactNumber(String expectedContactNumber) {
+        delayTime(TimeDelay.TIME_1000S);
         Assert.assertEquals(publicationContactNumber.getAttribute("value"), expectedContactNumber);
+    }
+
+    public void openRecentCreatedPublication() {
+        List<WebElement> publicationTable = driver.findElements(By.xpath("//*[@id='publications_table']//descendant::a"));
+        int numberOfPublication = publicationTable.size();
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        String publicationUrl = publicationTable.get(numberOfPublication - 1).getAttribute("href");
+        driver.get(publicationUrl);
+        delayTime(TimeDelay.TIME_2000S);
     }
 }
--- src/test/java/test_cases/pubication/PublicationCreation.java
+++ src/test/java/test_cases/pubication/PublicationCreation.java
@@ -1,21 +1,25 @@
 package test_cases.pubication;
 
 import base.TestBase;
+import constants.TimeDelay;
 import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PublicationCreation extends TestBase {
 
     @Test(description = "Verify \"Publication Creation\" Tab is present along with other Tabs on Left side and clickable")
     @Description("Verify \"Publication Creation\" Tab is present along with other Tabs on Left side and clickable")
-    public void verifyPublicationCreationIspresentLeftside(){
+    public void verifyPublicationCreationIspresentLeftside() {
         publications.openPublicaton();
         publications.verifyPublicationPageURL();
     }
 
     @Test(description = "Verify\"Publication Creation\" tab consists of following paramenters:")
     @Description("Verify\"Publication Creation\" tab consists of following paramenters:")
-    public void verifyPublicationCreationContainsAllParamenters(){
+    public void verifyPublicationCreationContainsAllParamenters() {
         publications.openPublicaton();
         publications.clkAddNewPublicationBtn();
         publications.verifyAllFiledAvailable();
@@ -23,7 +27,7 @@ public class PublicationCreation extends TestBase {
 
     @Test(description = "Create New Publication")
     @Description("Create New Publication")
-    public void createNewPublication(){
+    public void createNewPublication() {
         publications.openPublicaton();
         publications.clkAddNewPublicationBtn();
         publications.enterUniquePublicationName(publication.getProperty("PublicationName"));
@@ -33,13 +37,18 @@ public class PublicationCreation extends TestBase {
         publications.enterPublicationContactNumber(publication.getProperty("PublicationContactNumber"));
         publications.selectPublicationCountry(publication.getProperty("PublicationCountry"));
         publications.clkSaveBtn();
-        publications.verifyNoErrorWhileSaving();
         publications.verifyValidationMessage();
+        publications.openRecentCreatedPublication();
+        Assert.assertFalse(driver.findElement(By.id("metatagging_publication_name")).getAttribute("value").isEmpty());
+        Assert.assertFalse(driver.findElement(By.id("metatagging_publication_code")).getAttribute("value").isEmpty());
+        Assert.assertFalse(driver.findElement(By.id("metatagging_publication_description")).getAttribute("value").isEmpty());
+        Assert.assertFalse(driver.findElement(By.id("metatagging_publication_address")).getAttribute("value").isEmpty());
+        Assert.assertFalse(driver.findElement(By.id("metatagging_publication_phone_no")).getAttribute("value").isEmpty());
     }
 
     @Test(description = "Verify the error message by creating new publication with same name")
     @Description("Verify the error message by creating new publication with same name")
-    public void createPublicationWithExistingName(){
+    public void createPublicationWithExistingName() {
         publications.openPublicaton();
         publications.clkAddNewPublicationBtn();
         publications.enterPublicationName("Test3 publication");
@@ -51,9 +60,10 @@ public class PublicationCreation extends TestBase {
         publications.clkSaveBtn();
         publications.verifyErrorMessage();
     }
+
     @Test(description = "Verify Admin is able to enter Alphanumeric in Publication Name.")
     @Description("Verify Admin is able to enter Alphanumeric in Publication Name.")
-    public void VerifyAlphanumericInPublicationNameField(){
+    public void verifyAlphanumericInPublicationNameField() {
         publications.openPublicaton();
         publications.clkAddNewPublicationBtn();
         publications.enterPublicationName("12334 publication");
@@ -62,7 +72,7 @@ public class PublicationCreation extends TestBase {
 
     @Test(description = "Verify Admin is able to enter Alphanumeric in Publication Code.")
     @Description("Verify Admin is able to enter Alphanumeric in Publication Code.")
-    public void VerifyAlphanumericInPublicationCodeField(){
+    public void verifyAlphanumericInPublicationCodeField() {
         publications.openPublicaton();
         publications.clkAddNewPublicationBtn();
         publications.enterPublicationCode("12334 Code");
@@ -71,7 +81,7 @@ public class PublicationCreation extends TestBase {
 
     @Test(description = "Verify Admin is able to enter Alphanumeric in Publication Description.")
     @Description("Verify Admin is able to enter Alphanumeric in Publication Description.")
-    public void VerifyAlphanumericInPublicationDescriptionField(){
+    public void verifyAlphanumericInPublicationDescriptionField() {
         publications.openPublicaton();
         publications.clkAddNewPublicationBtn();
         publications.enterPublicationDescription("12334 Description");
@@ -80,7 +90,7 @@ public class PublicationCreation extends TestBase {
 
     @Test(description = "Verify Admin is able to enter Alphanumeric in Publication Address.")
     @Description("Verify Admin is able to enter Alphanumeric in Publication Address.")
-    public void VerifyAlphanumericInPublicationAddressField(){
+    public void verifyAlphanumericInPublicationAddressField() {
         publications.openPublicaton();
         publications.clkAddNewPublicationBtn();
         publications.enterPublicationAddress("12334 Address");
@@ -89,7 +99,7 @@ public class PublicationCreation extends TestBase {
 
     @Test(description = "Verify Admin is able to select the Country using the drop down box.")
     @Description("Verify Admin is able to select the Country using the drop down box.")
-    public void VerifyAbleToSelectCountryDropDown(){
+    public void verifyAbleToSelectCountryDropDown() {
         publications.openPublicaton();
         publications.clkAddNewPublicationBtn();
         publications.selectPublicationCountry(publication.getProperty("PublicationCountry"));
@@ -97,7 +107,7 @@ public class PublicationCreation extends TestBase {
 
     @Test(description = "Verify Admin is able to enter only numbers in Phone Number field")
     @Description("Verify Admin is able to enter only numbers in Phone Number field")
-    public void VerifyAbleToEnterNumbersInPhoneNumberField(){
+    public void verifyAbleToEnterNumbersInPhoneNumberField() {
         publications.openPublicaton();
         publications.clkAddNewPublicationBtn();
         publications.enterPublicationContactNumber("9876543210");
