89f79c98c78e95807fe5e28600a1f2e0abaff781
Surendar Kaliappan <Surendar@embibe.com>
2020-02-11 01:05:29 +0530

    Error Management test cases

---
+++ src/main/java/Pages/ErrorManagement.java
@@ -0,0 +1,134 @@
+package Pages;
+
+import base.TestBase;
+import constants.TimeDelay;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+import java.sql.Time;
+import java.util.List;
+
+public class ErrorManagement extends TestBase {
+
+    public ErrorManagement(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickErrorManagement() {
+        pageScrole(driver.findElement(By.xpath("//*[contains(text(),'Error Management')]")));
+        driver.findElement(By.xpath("//*[contains(text(),'Error Management')]")).click();
+        driver.navigate().to("https://new-content-admin-staging.embibe.com/errors");
+    }
+
+    public void clickCreateNewError() {
+        delayTime(TimeDelay.TIME_2000S);
+        driver.findElement(By.xpath("//button[@class='btn-success btn Ripple-parent header-action-button']")).click();
+    }
+
+    public void clickSaveNewError() {
+        delayTime(TimeDelay.TIME_2000S);
+        driver.findElement(By.xpath("//button[@class='btn-success btn Ripple-parent header-action-button']")).click();
+    }
+
+    public void enterErrorName(String ErrorName) {
+        delayTime(TimeDelay.TIME_2000S);
+        driver.findElement(By.xpath("((//*[@class='MuiInputBase-input MuiOutlinedInput-input'])[1])")).sendKeys(ErrorName);
+    }
+
+    public void enterErrorDescription(String ErrorDescription) {
+        delayTime(TimeDelay.TIME_1000S);
+        driver.findElement(By.xpath("//textarea[@class='MuiInputBase-input MuiOutlinedInput-input MuiInputBase-inputMultiline MuiOutlinedInput-inputMultiline']")).sendKeys(ErrorDescription);
+    }
+
+    public void enterErrorDeduction(String ErrorDeduction) {
+        delayTime(TimeDelay.TIME_1000S);
+        driver.findElement(By.xpath("((//*[@class='MuiInputBase-input MuiOutlinedInput-input'])[2])")).sendKeys(ErrorDeduction);
+    }
+
+    public void enterErrorMaxDeduction(String ErrorMaxDeduction) {
+        delayTime(TimeDelay.TIME_1000S);
+        driver.findElement(By.xpath("((//*[@class='MuiInputBase-input MuiOutlinedInput-input'])[3])")).sendKeys(ErrorMaxDeduction);
+    }
+
+    public void clickContentTypeDropDown() {
+        driver.findElement(By.xpath("((//*[@id='simple-select-outlined'])[1])")).click();
+        delayTime(TimeDelay.TIME_2000S);
+        List<WebElement> contentType = driver.findElement(By.xpath("//ul[@class='MuiList-root MuiMenu-list MuiList-padding']")).findElements(By.tagName("li"));
+        contentType.get(1).click(); //Click Question
+    }
+
+    public void clickSubSectionDropDown() {
+        driver.findElement(By.xpath("((//*[@id='simple-select-outlined'])[2])")).click();
+        List<WebElement> subSection = driver.findElement(By.xpath("//ul[@class='MuiList-root MuiMenu-list MuiList-padding']")).findElements(By.tagName("li"));
+        subSection.get(1).click(); //Click Question
+    }
+
+    public void clickErrorTypeDropDown() {
+        driver.findElement(By.xpath("((//*[@id='simple-select-outlined'])[3])")).click();
+        List<WebElement> errorType = driver.findElement(By.xpath("//ul[@class='MuiList-root MuiMenu-list MuiList-padding']")).findElements(By.tagName("li"));
+        errorType.get(1).click(); //Click Question
+    }
+
+    public void clickErrorMultiplierDropDown() {
+        driver.findElement(By.xpath("((//*[@id='simple-select-outlined'])[4])")).click();
+        List<WebElement> errorMultiplier = driver.findElement(By.xpath("//ul[@class='MuiList-root MuiMenu-list MuiList-padding']")).findElements(By.tagName("li"));
+        errorMultiplier.get(1).click(); //Click Question
+    }
+
+    public void clickErrorSeverityDropDown() {
+        driver.findElement(By.xpath("((//*[@id='simple-select-outlined'])[5])")).click();
+        List<WebElement> errorSeverity = driver.findElement(By.xpath("//ul[@class='MuiList-root MuiMenu-list MuiList-padding']")).findElements(By.tagName("li"));
+        errorSeverity.get(1).click(); //Click Question
+    }
+
+    public void closeCreateError() {
+        driver.findElement(By.xpath("//*[@class='MuiSvgIcon-root alert-close-back-to-listing']")).click();
+    }
+
+    public void getErrorMessage(String expectedError) {
+        delayTime(TimeDelay.TIME_500S);
+        String errorMessage = driver.findElement(By.className("header-alert")).getText();
+        Assert.assertEquals(errorMessage, expectedError);
+    }
+
+    public void verifyErrorName(String ExpectedErrorName) {
+        String ErrorName = driver.findElement(By.xpath("((//*[@class='MuiInputBase-input MuiOutlinedInput-input'])[1])")).getAttribute("value");
+        Assert.assertEquals(ErrorName, ExpectedErrorName);
+    }
+
+    public void verifyErrorDescription(String ExpectedErrorDescription) {
+        delayTime(TimeDelay.TIME_1000S);
+        String ErrorDescription = driver.findElement(By.xpath("//textarea[@class='MuiInputBase-input MuiOutlinedInput-input MuiInputBase-inputMultiline MuiOutlinedInput-inputMultiline']")).getText();
+        Assert.assertEquals(ErrorDescription, ExpectedErrorDescription);
+    }
+
+    public void verifyErrorDeduction(String ExpectedErrorDeduction) {
+        delayTime(TimeDelay.TIME_1000S);
+        String ErrorDeduction = driver.findElement(By.xpath("((//*[@class='MuiInputBase-input MuiOutlinedInput-input'])[2])")).getAttribute("value");
+        Assert.assertEquals(ErrorDeduction, ExpectedErrorDeduction);
+    }
+
+    public void verifyErrorMaxDeduction(String ExpectedErrorMaxDeduction) {
+        delayTime(TimeDelay.TIME_1000S);
+        String ErrorMaxDeduction = driver.findElement(By.xpath("((//*[@class='MuiInputBase-input MuiOutlinedInput-input'])[3])")).getAttribute("value");
+        Assert.assertEquals(ErrorMaxDeduction, ExpectedErrorMaxDeduction);
+    }
+
+    public void enterErrorCode(String errorCode) {
+        driver.findElement(By.xpath("//*[@class='MuiInputBase-input MuiOutlinedInput-input']")).sendKeys(errorCode);
+    }
+
+    public void clickCreatorInField() {
+        driver.findElement(By.id("simple-select-outlined")).click();
+        List<WebElement> creator = driver.findElement(By.xpath("//ul[@class='MuiList-root MuiMenu-list MuiList-padding']")).findElements(By.tagName("li"));
+        creator.get(1).click();
+    }
+
+    public void clickSearch() {
+        driver.findElement(By.xpath("//*[@class='btn-success btn Ripple-parent search-button']")).click();
+    }
+}
---
+++ src/test/java/errorsManagement/ErrorCreation.java
@@ -0,0 +1,177 @@
+package errorsManagement;
+
+import base.TestBase;
+import constants.TimeDelay;
+import io.qameta.allure.Description;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.Categories;
+
+public class ErrorCreation extends TestBase {
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify by creating new error for questions")
+    @Description("Verify by creating new error for questions")
+    public void verifyByCreatingNewError() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        errorManagement.enterErrorName("New Error - Test Automation");
+        errorManagement.clickContentTypeDropDown();
+        errorManagement.enterErrorDescription("Error Description - Test Automation");
+        errorManagement.clickSubSectionDropDown();
+        errorManagement.clickErrorTypeDropDown();
+        errorManagement.clickErrorMultiplierDropDown();
+        errorManagement.clickErrorSeverityDropDown();
+        errorManagement.enterErrorDeduction("5");
+        errorManagement.enterErrorMaxDeduction("10");
+        errorManagement.clickSaveNewError();
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify by clicking on the error management in the side bar")
+    @Description("Verify by clicking on the error management in the side bar")
+    public void verifyByClickingErrorManagementSideBar() {
+        errorManagement.clickErrorManagement();
+        String currentURL = driver.getCurrentUrl();
+        Assert.assertEquals(currentURL, "https://new-content-admin-staging.embibe.com/errors");
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify by clicking on the \"Create New Error\" button on the error management listing page")
+    @Description("Verify by clicking on the \"Create New Error\" button on the error management listing page")
+    public void verifyByClickingCreateNewErrorButton() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        String currentURL = driver.getCurrentUrl();
+        Assert.assertEquals(currentURL, "https://new-content-admin-staging.embibe.com/errors/new");
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify by click on the close button(X) under the Create New Error page")
+    @Description("Verify by click on the close button(X) under the Create New Error page")
+    public void verifyByClickCloseButtonInCreateNewError() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        errorManagement.closeCreateError();
+        String currentURL = driver.getCurrentUrl();
+        Assert.assertEquals(currentURL, "https://new-content-admin-staging.embibe.com/errors");
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify the error message by clcking on the save without any data")
+    @Description("Verify the error message by clcking on the save without any data")
+    public void verifyErrorMessageBySaveWithoutAnyData() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        errorManagement.clickSaveNewError();
+        errorManagement.getErrorMessage("One or more fields are empty");
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify user should be able to enter alaphanumeric in the \"Error Name\" field")
+    @Description("Verify user should be able to enter alaphanumeric in the \"Error Name\" field")
+    public void verifyAbleToEnterAlaphanumericInErrorNameField() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        errorManagement.enterErrorName("Error922");
+        errorManagement.verifyErrorName("Error922");
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify the list available by clicking on the \"Content Type\" drop down.")
+    @Description("Verify the list available by clicking on the \"Content Type\" drop down.")
+    public void verifyListAvailableonContentTypeDropDown() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        errorManagement.clickContentTypeDropDown();
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify user should be able to enter alaphanumeric in the \"Error Description\" field")
+    @Description("Verify user should be able to enter alaphanumeric in the \"Error Description\" field")
+    public void verifyUserShouldAbleToEnterAlaphanumericInErrorDescription() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        errorManagement.enterErrorDescription("Error Description 1234");
+        errorManagement.verifyErrorDescription("Error Description 1234");
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify user should be able to select the \"Sub Section\" on the dropdown")
+    @Description("Verify user should be able to select the \"Sub Section\" on the dropdown")
+    public void verifyUserShouldAbleToSelectSubSectionOnTheDropdown() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        errorManagement.clickSubSectionDropDown();
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify user should be able to select the \"Error Type\" on the dropdown")
+    @Description("Verify user should be able to select the \"Error Type\" on the dropdown")
+    public void verifyUserShouldAbleToSelectErrorTypeOnTheDropdown() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        errorManagement.clickErrorTypeDropDown();
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify user should be able to select the \"Error Multiplier\" on the dropdown")
+    @Description("Verify user should be able to select the \"Error Multiplier\" on the dropdown")
+    public void verifyUserShouldAbleToSelectErrorMultiplierOnTheDropdown() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        errorManagement.clickErrorMultiplierDropDown();
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify user should be able to select the \"Severity\" on the dropdown")
+    @Description("Verify user should be able to select the \"Severity\" on the dropdown")
+    public void verifyUserShouldAbleToSelectSeverityOnTheDropdown() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        errorManagement.clickErrorSeverityDropDown();
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify by entering the alaphebets in the \"Deduction\" box")
+    @Description("Verify by entering the alaphebets in the \"Deduction\" box")
+    public void verifyEnteringAlaphebetsInDeductionbox() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        errorManagement.enterErrorDeduction("Alpha");
+        errorManagement.verifyErrorDeduction("");
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify by entering the negative values in the \"Deduction\" tab")
+    @Description("Verify by entering the negative values in the \"Deduction\" tab")
+    public void verifyEnteringNegativeValuesInDeduction() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        errorManagement.enterErrorDeduction("-1");
+        errorManagement.verifyErrorDeduction("1");
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify by entering the numerical value in the deduction tab")
+    @Description("Verify by entering the numerical value in the deduction tab")
+    public void verifyEnteringNumericalValueDeduction() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        errorManagement.enterErrorDeduction("87");
+        errorManagement.verifyErrorDeduction("87");
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify by entering the alaphebets in the \"Max Deduction\" box")
+    @Description("Verify by entering the alaphebets in the \"Max Deduction\" box")
+    public void verifyEnteringAlaphebetsInMaxDeductionbox() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        errorManagement.enterErrorMaxDeduction("Alpha");
+        errorManagement.verifyErrorMaxDeduction("");
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify by entering the negative values in the \"Max Deduction\" tab")
+    @Description("Verify by entering the negative values in the \"Deduction\" tab")
+    public void verifyEnteringNegativeValuesInMaxDeduction() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        errorManagement.enterErrorMaxDeduction("-1");
+        errorManagement.verifyErrorMaxDeduction("1");
+    }
+
+    @Test(groups = {Categories.ICMS_SMOKE_TEST_CASE, Categories.ICMS_ERRORMANAGEMENT}, description = "Verify by entering the numerical value in the Max deduction tab")
+    @Description("Verify by entering the numerical value in the deduction tab")
+    public void verifyEnteringNumericalValueMaxDeduction() {
+        errorManagement.clickErrorManagement();
+        errorManagement.clickCreateNewError();
+        errorManagement.enterErrorMaxDeduction("87");
+        errorManagement.verifyErrorMaxDeduction("87");
+    }
+
+}
